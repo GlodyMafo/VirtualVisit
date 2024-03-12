809faf0f3c3d8a428947fab1c62f0d0a38d7e628
Glody Mafo <glodymafokis@gmail.com>
2024-03-12 17:42:31 +0200

    adding responsive

--- main.js
+++ main.js
@@ -108,6 +108,13 @@ const animate = () => {
 
 animate();
 
+window.addEventListener('resize',()=>{
+	camera.aspect=window.innerWidth/window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+})
+
 
 
 
