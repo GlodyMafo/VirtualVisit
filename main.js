6ece671952ae0ed363a8c760df8ed0c5320675f0
Glody Mafo <glodymafokis@gmail.com>
2024-03-13 17:46:18 +0200

    adding object

--- main.js
+++ main.js
@@ -5,14 +5,15 @@ import * as lilGui from "lil-gui"
 import gsap from "gsap"
 
 
-
 const scene = new THREE.Scene();
 
 // Position initiale de la camera
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(2.1, 0.98, -5.6)
-camera.rotation.set(-2.9, 0.35, 3.86)
+camera.position.set(7.66, 0.24, -1.66)
+camera.rotation.set(-2.75, 1.33, 2.76)
+
+// camera.position.z=5
 
 
 scene.add(camera);
@@ -25,12 +26,12 @@ document.body.appendChild(renderer.domElement);
 
 // controls.enableDamping = true;
 
-let position=0;
-
+let position = 0;
 
 
 const gltfLoader = new GLTFLoader();
 gltfLoader.load("public/scene.gltf", (gltf) => {
+  console.log(gltf);
   const model = gltf.scene;
   scene.add(model);
 
@@ -40,28 +41,33 @@ gltfLoader.load("public/scene.gltf", (gltf) => {
   //   console.log(camera.rotation);
   // })
 
-  window.addEventListener("mouseup",function(){
-    switch(position){
-      case 0: 
-      cameraMove(0.56,1.6,-1.26);
-      cameraRotation(-2.96,0.35,3.08)
-      position=1
-      break;
+  window.addEventListener("mouseup", function () {
+    switch (position) {
+      case 0:
+        cameraMove(-0.49, 1.85, -2.08);
+        cameraRotation(1.26, 1.51, -1.25)
+        position = 1
+        break;
 
-      case 1: 
-      cameraMove(-1.88,1.57,-0.43);
-      cameraRotation(-2.93,0.44,3.05)
-      position=2
-      break;
-      
+      case 1:
+        cameraMove(-1.52, 0.32, 2.19);
+        cameraRotation(-3.13, 1.31, 3.13)
+        position = 2
+        break;
+
       case 2:
-      cameraMove(-1.97,1.69,1.31);
-      cameraRotation(-0.11,0.66,0.07)
-      position=3
-      break;
+        cameraMove(0.10, 1.26, 0.09);
+        cameraRotation(0.12, 0.57, 0.06)
+        position = 3
+        break;
+
+      case 3:
+        cameraMove(-3.34, 9.56, -1.40);
+        cameraRotation(-1.57, -0.68, -1.57)
+        position = 4
+        break;
     }
   })
-
 
 
   // GUI pour recupérer les valeurs sur l'écran
@@ -83,21 +89,21 @@ gltfLoader.load("public/scene.gltf", (gltf) => {
   // .stop(0.001)
 })
 
-function cameraMove (x,y,z){
-  gsap.to(camera.position,{
-   x,
-   y,
-   z,duration:3
+function cameraMove(x, y, z) {
+  gsap.to(camera.position, {
+    x,
+    y,
+    z, duration: 3
   })
 }
 
-function cameraRotation(x,y,z){
-gsap.to(camera.rotation,{
-x,
-y,
-z,
-duration:3,
-})
+function cameraRotation(x, y, z) {
+  gsap.to(camera.rotation, {
+    x,
+    y,
+    z,
+    duration: 3,
+  })
 }
 
 const animate = () => {
@@ -106,12 +112,12 @@ const animate = () => {
   window.requestAnimationFrame(animate);
 }
 
-animate();
+animate()
 
-window.addEventListener('resize',()=>{
-	camera.aspect=window.innerWidth/window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize(window.innerWidth, window.innerHeight);
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
 })
 
